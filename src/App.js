65eab6e3d487422675a881ff4646d69ca2f3002b
khushbusingh1029-ship-it <khushbusingh1029@gmail.com>
2026-07-08 14:20:25 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Home from "./Home";
+import Quiz from "./Quiz";
+import { quizData } from "./data";
+import "./App.css";
 
 function App() {
+  const [section, setSection] = useState(null);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {!section ? (
+        <Home startQuiz={setSection} />
+      ) : (
+        <Quiz
+          data={quizData[section]}
+          goHome={() => setSection(null)}
+        />
+      )}
+      
     </div>
   );
 }
